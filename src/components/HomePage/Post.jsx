c5875ce4c24b75042e8e3282548a6d89447a54d6
AltairGeo <altairgeo000@gmail.com>
2025-01-19 22:31:36 +0300

    transfer file

--- src/components/HomePage/Post.jsx
+++ src/components/HomePage/Post.jsx
@@ -1,6 +1,19 @@
 import './Post.css'
+import { useEffect, useState } from 'react'
 
 export default function Post(props){
+    const [authorPost, authorSet] = useState(null)
+    useEffect(() => {
+        async function fetch_user() {
+            const resp = await fetch("http://127.0.0.1:8000/users/get_user?id=1")
+            if(resp.ok){
+                const data = await resp.json()
+                authorSet(data.nickname)
+            }
+        }
+        fetch_user()
+    }, [])
+
     return (
         <li id='PostElement'>
             <div>
@@ -9,7 +22,7 @@ export default function Post(props){
                 <div class="PostBottom">
                     <button class="btn-read-more">Читать дальше</button>
                     <span class="PostSpan">
-                        <p id="PostAuthorName">{props.author}</p>
+                        <p id="PostAuthorName">{authorPost}</p>
                     </span>
                 </div>
             </div>
