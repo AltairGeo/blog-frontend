8d89c3c305083696e7a8a0f9b5c635a57bdb9ffb
AltairGeo <altairgeo000@gmail.com>
2025-01-25 01:01:15 +0300

    fix

--- src/components/HomePage/Post.jsx
+++ src/components/HomePage/Post.jsx
@@ -1,11 +1,12 @@
 import './Post.css'
 import { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
 
 export default function Post(props){
     const [authorPost, authorSet] = useState(null)
     useEffect(() => {
         async function fetch_user() {
-            const resp = await fetch("http://127.0.0.1:8000/users/get_user?id=1")
+            const resp = await fetch(`http://127.0.0.1:8000/users/get_user?id=${props.author}`)
             if(resp.ok){
                 const data = await resp.json()
                 authorSet(data.nickname)
@@ -18,7 +19,8 @@ export default function Post(props){
         <li id='PostElement'>
             <div>
                 <div className="PostHeader"><h1>{props.title}</h1></div>
-                <p id="PostBody">{props.body.slice(0, 350) + "..."}</p>
+                {/* <p id="PostBody">{props.body.slice(0, 350) + "..."}</p> */}
+                <div id="PostBody"><Markdown>{props.body.slice(0, 350) + "..."}</Markdown></div>
                 <div className="PostBottom">
                     <button className="btn-read-more">Читать дальше</button>
                     <span className="PostSpan">
