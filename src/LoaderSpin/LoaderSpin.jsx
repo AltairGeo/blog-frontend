3cb61c1c51287f67b73d2f1a11a9b528b991340a
AltairGeo <altairgeo000@gmail.com>
2025-03-23 18:43:09 +0300

    micro-fix

--- src/LoaderSpin/LoaderSpin.jsx
+++ src/LoaderSpin/LoaderSpin.jsx
@@ -2,6 +2,6 @@ import './LoaderSpin.css';
 
 export default function LoaderSpin() {
     return (
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     )
 }
